64896cb5d0ecdccfa816fa4c8c4463a83c8ad11b
phiathao <phiat11@gmail.com>
2018-12-19 15:28:16 -0600

    salt and hash + build

---
+++ build/precache-manifest.68819f079f97a636f8cef3564c52609d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8b8684782575b556afb",
+    "url": "/static/css/main.6cf8a8f6.chunk.css"
+  },
+  {
+    "revision": "a8b8684782575b556afb",
+    "url": "/static/js/main.a8b86847.chunk.js"
+  },
+  {
+    "revision": "ba01b7120d1b4950e309",
+    "url": "/static/js/1.ba01b712.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  },
+  {
+    "revision": "12d2f96206737a5e6155cc6c293dedcc",
+    "url": "/index.html"
+  }
+];
